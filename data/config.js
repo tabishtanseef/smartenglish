de275d617b0dd0148c15dcff45e46b90c4cd4eac
Tabish Tanseef <tabishtanseef@gmail.com>
2019-09-03 12:45:02 +0530

    smart english

---
+++ data/config.js
@@ -0,0 +1,56 @@
+var fPage = 1;
+						 const appdb = {
+						'config': {
+							'subject': 'Smart English 8 ch 8',
+							'class': 'Class 6',
+							'id': 'gud_335',
+							'totalPages': 19,
+							'bookWidth': 2570,
+							'bookHeight': 3300
+						},
+						'ebook': {'toc': {
+							'icon': 'img/content.png',
+							'menu': 'Table of Contents',
+							'link': 'content',
+							'data': [{
+						'page': 6,
+						'title': 'Chapter 8 - The Special Prize'
+					}]},'Animation': {
+									'icon': 'img/animation.png',
+									'menu': 'Animation',
+									'link': 'video',
+									'data': [{
+							'path': 'resources/Animation/SE8_ani_8_1.mp4',
+							'title': 'Chapter 8 — Animation',
+							'size': '1200x2000',
+							'page': 6}]},'Interactivities': {
+								'icon': 'img/interactivity.png',
+								'menu': 'Interactivities',
+								'link': 'iframe',
+								'data': [
+									{
+								'path': '',
+								'title': 'Actvity',
+								'page': 9,
+								'interactivities':[
+									{
+							'path': 'resources/Actvity/Ch_8_Cati/index.html',
+							'title': 'Activity',
+							'size': '1200x2000',
+							'page': 9}
+									],
+								'size': '1200x2000',
+								}
+								]}
+								,'zother': [
+							{
+							'icon': 'img/test_generator.png',
+							'title': 'Teacher\'s Manual',
+							'path': 'resources/TM Smart Eng-8tm.pdf',
+							'size': '1200x2000',
+							'link': 'iframe'
+						}
+						]
+						
+					}
+				}
---
+++ data/page.js
@@ -0,0 +1,17 @@
+const PAGES_LINKING = {'6':[{			 
+											x: 77,
+											y: 9,
+											title: 'Animation',
+											icon: 'img/icons/Animation.png',
+											link: 'resources/Animation/SE8_ani_8_1.mp4',
+											type: 'video',
+											'size': '1000x760'
+											}],'9':[{			 
+											x: 47,
+											y: 67,
+											title: 'Actvity',
+											icon: 'img/icons/Activity.png',
+											link: 'resources/Actvity/Ch_8_Cati/index.html',
+											type: 'iframe',
+											'size': '1200x2000'
+											}]}
